29cedbd39f565b75d69892c5898fa0f06903a4d6
Qiusheng Wu <giswqs@gmail.com>
2023-03-07 08:12:32 -0500

    Fixed error

--- gee_catalog.py
+++ gee_catalog.py
@@ -17,68 +17,74 @@ for link in root.get_child_links():
 
 datasets = []
 
+i = 0
 for catalog in catalogs:
-
     cat = Client.open(catalog, headers=[])
     print(cat.title)
 
-    for collection in cat.get_all_collections():
-        data = collection.to_dict()
-        print(data["id"])
-        dataset = {}
-        output = out_dir + "/" + data["id"].replace("/", "_") + ".json"
-        if not os.path.exists(os.path.dirname(output)):
-            os.makedirs(os.path.dirname(output))
-        with open(output, "w") as f:
-            json.dump(data, f, indent=4)
+    try:
+        for index, collection in enumerate(cat.get_all_collections()):
+            data = collection.to_dict()
+            print(f'{i}: {data["id"]}')
+            i = i + 1
+            dataset = {}
+            output = out_dir + "/" + data["id"].replace("/", "_") + ".json"
+            if not os.path.exists(os.path.dirname(output)):
+                os.makedirs(os.path.dirname(output))
+            with open(output, "w") as f:
+                json.dump(data, f, indent=4)
 
-        dataset["id"] = data["id"]
-        dataset["title"] = data["title"]
-        dataset["type"] = data["gee:type"]
-        if dataset["type"] == "image":
-            dataset["snippet"] = f"ee.Image('{dataset['id']}')"
-        elif dataset["type"] == "image_collection":
-            dataset["snippet"] = f"ee.ImageCollection('{dataset['id']}')"
-        else:
-            dataset["snippet"] = f"ee.FeatureCollection('{dataset['id']}')"
+            dataset["id"] = data["id"]
+            dataset["title"] = data["title"]
+            dataset["type"] = data["gee:type"]
+            if dataset["type"] == "image":
+                dataset["snippet"] = f"ee.Image('{dataset['id']}')"
+            elif dataset["type"] == "image_collection":
+                dataset["snippet"] = f"ee.ImageCollection('{dataset['id']}')"
+            else:
+                dataset["snippet"] = f"ee.FeatureCollection('{dataset['id']}')"
 
-        dataset["provider"] = data["providers"][0]["name"].replace("\n", " ")
-        dataset["state_date"] = data["extent"]["temporal"]["interval"][0][0].split("T")[
-            0
-        ]
-        dataset["end_date"] = data["extent"]["temporal"]["interval"][0][1].split("T")[0]
-        dataset["bbox"] = ", ".join(
-            [str(coord) for coord in data["extent"]["spatial"]["bbox"][0]]
-        )
-        if "deprecated" in data:
-            dataset["deprecated"] = data["deprecated"]
-        else:
-            dataset["deprecated"] = False
-        dataset["keywords"] = ", ".join(data["keywords"])
+            dataset["provider"] = data["providers"][0]["name"].replace("\n", " ")
+            dataset["state_date"] = data["extent"]["temporal"]["interval"][0][0].split(
+                "T"
+            )[0]
+            dataset["end_date"] = data["extent"]["temporal"]["interval"][0][1].split(
+                "T"
+            )[0]
+            dataset["bbox"] = ", ".join(
+                [str(coord) for coord in data["extent"]["spatial"]["bbox"][0]]
+            )
+            if "deprecated" in data:
+                dataset["deprecated"] = data["deprecated"]
+            else:
+                dataset["deprecated"] = False
+            dataset["keywords"] = ", ".join(data["keywords"])
 
-        link = ""
-        thumbnail = ""
-        terms_of_use = ""
-        script = ""
+            link = ""
+            thumbnail = ""
+            terms_of_use = ""
+            script = ""
 
-        for l in data["links"]:
-            if l["rel"] == "self":
-                link = l["href"]
-            if l["rel"] == "preview":
-                thumbnail = l["href"]
-            if l["rel"] == "license":
-                terms_of_use = l["href"]
-            if l["rel"] == "related":
-                script = l["href"]
-        dataset["catalog"] = link
-        url_prefix = "https://developers.google.com/earth-engine/datasets/catalog"
-        dataset["url"] = f"{url_prefix}/{data['id'].replace('/', '_')}"
-        dataset["thumbnail"] = thumbnail
-        dataset["script"] = script
-        dataset["terms_of_use"] = terms_of_use
-        dataset["license"] = data["license"]
+            for l in data["links"]:
+                if l["rel"] == "self":
+                    link = l["href"]
+                if l["rel"] == "preview":
+                    thumbnail = l["href"]
+                if l["rel"] == "license":
+                    terms_of_use = l["href"]
+                if l["rel"] == "related":
+                    script = l["href"]
+            dataset["catalog"] = link
+            url_prefix = "https://developers.google.com/earth-engine/datasets/catalog"
+            dataset["url"] = f"{url_prefix}/{data['id'].replace('/', '_')}"
+            dataset["thumbnail"] = thumbnail
+            dataset["script"] = script
+            dataset["terms_of_use"] = terms_of_use
+            dataset["license"] = data["license"]
 
-        datasets.append(dataset)
+            datasets.append(dataset)
+    except Exception as e:
+        print(e)
 
 print("Total datasets: ", len(datasets))
 
